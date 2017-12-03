38652e4f7d08aca4f0b095539ed067ba046b854f
Katherine <krosenk729@gmail.com>
2017-12-03 15:27:46 -0500

    Updated to tweak a few css things and add a modal for game over

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-	//initialize variables
+	// initialize variables
 	var cafgoal = 1, cafbuttons = [1, 1, 1, 1];
 	var cafwins = 0, cafloses = 0, cafuserscore = 0;
 
-	//function to initialize a game
+	// function to initialize a game
 	function gameSetup(){
 		//Set goal between 19 and 120
 		cafgoal = Math.floor((Math.random() * 120) + 19); 
@@ -15,29 +15,34 @@ $(document).ready(function(){
 		cafuserscore = 0;
 		$('#cafuser').html(cafuserscore);
 		$('#cafgoal').html(cafgoal);
-		/* console.log(cafbuttons); */
+
 	}
-	//initialize game on load
+	// initialize game on load
 	gameSetup();
 
-	//when a button is clicked
+	// function to react when a button is clicked
 	$('.game-control').click(function(){
-		//update score
-		cafuserscore += cafbuttons[$(this).data("cafbutton") - 1];
-		$('#cafuser').html(cafuserscore); 
 
-		//if win, notify and reset
+		// update score based on assigned data attribute 
+		// -1 because index of buttons starts at 1 
+		cafuserscore += cafbuttons[$(this).data("cafbutton") - 1];
+		$('#cafuser').html(cafuserscore);
+		$('#cafuser').animate({fontSize: '1.5em'}, {duration: 100}).animate({fontSize: '1em'}, {duration: 50});
+
+		// if win, notify and reset
 		if(cafuserscore == cafgoal){
 			cafwins ++;
 			$('#cafwins').html(cafwins);
+			$('.modal').toggleClass('modal-shown');
 			gameSetup();
 		}
 		//if lose, notify and reset
 		else if(cafuserscore > cafgoal){
 			cafloses ++;
 			$('#cafloses').html(cafloses);
+			$('.modal').toggleClass('modal-shown');
 			gameSetup();
 		}
-	}); 
+	});
 
 });
